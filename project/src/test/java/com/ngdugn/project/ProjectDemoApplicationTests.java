3f6326c1db7f7c3afdf81d8803e9b0e531887a7d
Ngoc Dung <dungktn03@gmail.com>
2024-04-15 07:54:29 +0700

    Upload new project

---
+++ project/src/test/java/com/ngdugn/project/ProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngdugn.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
